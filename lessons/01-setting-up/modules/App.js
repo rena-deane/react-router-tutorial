2a99c5692b623b6e7cd50cc14a167216b3bb4688
rich-farr <scarletmanuka@gmail.com>
2016-07-07 11:02:56 +1200

    Add some shiz

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router';
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+    	<div>
+    		<h1>Ghetto hub issues</h1>
+    		<ul role="nav">
+    			<li><Link to='/about' activeStyle={{ color:'#c9ce06' }}>About</Link></li>
+    			<li><Link to='/repos' activeStyle={{ color:'chartreuse' }}>Repos</Link></li>
+    		</ul>
+    		{this.props.children}
+    	</div>
+    )
   }
 })
