920ca5d1d31edc57a885511bcccffa35bc5557b4
rich-farr <scarletmanuka@gmail.com>
2016-07-07 16:53:33 +1200

    Complete Lesson 10: Clean URLs

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
 import Home from './modules/Home'
 
 render((
-	<Router history={hashHistory}>
+	<Router history={browserHistory}>
 
 		<Route path="/" component={App}>
 			<IndexRoute component={Home}/>
